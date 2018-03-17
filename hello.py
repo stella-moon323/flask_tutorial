c35489b328630e83bc3806f13e650c200ab066b3
stella-moon323 <aquasky0323@gmail.com>
2018-03-17 15:46:34 +0900

    db

--- hello.py
+++ hello.py
@@ -1,11 +1,23 @@
-from flask import  Flask, render_template
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hello.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+
+    def __init__(self, username):
+        self.username = username
 
 @app.route('/')
 def hello():
-    return render_template('hello.html')
+    user_list = User.query.all()
+    return render_template('hello.html', user_list=user_list)
 
 
 if __name__ == '__main__':
